6f083013c4434983ae57a299d5044a32366c7af8
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-02-08 01:37:55 +0900

    Introduce mutex

--- sleep_sort.cpp
+++ sleep_sort.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <mutex>
 
 void test_normal();
 
@@ -14,11 +15,14 @@ int main() {
 void sleep_sort(std::vector<int> &v) {
     std::vector<int> r;
     std::vector<std::thread> ths;
+    std::mutex mtx;
     for (auto i : v) {
         ths.push_back(std::thread(
             [&](int s) {
                 std::this_thread::sleep_for(std::chrono::seconds(s));
+                mtx.lock();
                 r.push_back(s);
+                mtx.unlock();
             },
             i));
     }
