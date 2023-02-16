e15f1025c5e22dd9aa63758f91a465a564b10a6c
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-02-16 13:18:26 +0900

    Change function siginiture.

--- sleep_sort_test.cpp
+++ sleep_sort_test.cpp
@@ -1,7 +1,24 @@
 #include "sleep_sort.h"
 
-static void test_sort(const std::string msg, std::vector<int> want,
-                      std::vector<int> sample) {
+struct Unit {
+    std::string msg;
+    std::vector<int> want;
+    std::vector<int> sample;
+};
+static void test_sort(Unit &u);
+
+void test_normal() {
+    std::vector<Unit> tests = {
+        Unit{"one element"s, {1}, {1}}, Unit{"two elements"s, {1, 2}, {2, 1}},
+        Unit{"five elements"s, {1, 2, 3, 4, 5}, {5, 4, 2, 3, 1}},
+        Unit{"duplicated values"s, {1, 1, 2, 2}, {2, 1, 1, 2}}};
+
+    for (auto u : tests) {
+        test_sort(u);
+    }
+}
+
+static void test_sort(Unit &u) {
     auto pp = [](std::vector<int> v) {
         std::cout << "["s;
         auto delim = ""s;
@@ -12,21 +29,14 @@ static void test_sort(const std::string msg, std::vector<int> want,
         std::cout << "]"s;
     };
 
-    sleep_sort(sample);
+    sleep_sort(u.sample);
 
-    if (want != sample) {
-        std::cout << msg << ": want"s;
-        pp(want);
+    if (u.want != u.sample) {
+        std::cout << u.msg << ": want"s;
+        pp(u.want);
         std::cout << ", got"s;
-        pp(sample);
+        pp(u.sample);
         std::cout << std::endl;
         exit(1);
     }
-}
-
-void test_normal() {
-    test_sort("one element"s, {1}, {1});
-    test_sort("two elements"s, {1, 2}, {2, 1});
-    test_sort("five elements"s, {1, 2, 3, 4, 5}, {5, 4, 2, 3, 1});
-    test_sort("duplicated values"s, {1, 1, 2, 2}, {2, 1, 1, 2});
 }
